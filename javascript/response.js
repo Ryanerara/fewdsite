1469b2cabe5f1daebb14b1549567c24aa5bcb8ff
Ryane Ridenour <ryane.ashli@gmail.com>
2015-05-31 18:48:05 -0400

    Fix slick file paths

--- javascript/response.js
+++ javascript/response.js
@@ -19,14 +19,14 @@ $(document).ready(function() {
 	);
 });
 
-
+/* unsure about this
 $('.fade').slick({
   dots: true,
   infinite: true,
   speed: 500,
   fade: true,
   cssEase: 'linear'
-});
+});*/
 
 
 
